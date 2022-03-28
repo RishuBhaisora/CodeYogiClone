fba5d073919a7a2c99a319f42d50d153685c0ffe
Rishabh  Bhaisora <102508172+RishuBhaisora@users.noreply.github.com>
2022-03-28 07:36:42 +0000

    first commit to github

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,31 @@ import React from 'react';
 import SideBar from './SideBar';
 import LecturesList from './LecturesList';
 import AssignmentList from './AssignmentList';
-import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import NoPage from './NoPage';
 import QuizPage from './QuizPage';
 import AssignmentDetailList from './AssignmentDetailList';
 import ProfilePage from './ProfilePage';
 
 function App() {
-  
-  return (
-   <BrowserRouter>
-      <Routes>
-        
-          <Route path="quiz" element={<QuizPage />} />
-          <Route path="/" element={<SideBar/>}>
-          <Route index element={<Navigate to="/lectures" />} /> 
-          <Route path="assignments" element={<AssignmentList />} />
-          <Route path="lectures" element={<LecturesList />} />
-          <Route path="profile" element={<ProfilePage />} />  
-          <Route path="assignments/:id/detail" element={<AssignmentDetailList />} />
-        </Route>
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="quiz" element={<QuizPage />} />
+				<Route path="/" element={<SideBar />}>
+					<Route index element={<Navigate to="/lectures" />} />
+					<Route path="assignments" element={<AssignmentList />} />
+					<Route path="lectures" element={<LecturesList />} />
+					<Route path="profile" element={<ProfilePage />} />
+					<Route
+						path="assignments/:id/detail"
+						element={<AssignmentDetailList />}
+					/>
+				</Route>
+				<Route path="*" element={<NoPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
-
- 
