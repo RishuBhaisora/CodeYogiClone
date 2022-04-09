e6331727b335c4deb31a0acbc918d8bfa3d10275
Rishabh  Bhaisora <102508172+RishuBhaisora@users.noreply.github.com>
2022-04-09 14:52:20 +0000

    Api test error fixing

--- src/QuizPage.jsx
+++ src/QuizPage.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Link} from "react-router-dom";
 import axios from "axios";
 function QuizPage() {
-  const [responseData , setData]=React.useState([]);
   const token = axios.get("https://api.codeyogi.io/batches/1/sessions",{
     withCredentials:true,
   });
   token.then((response)=>{
-    setData(response.data);
-    console.log(response.data)
+  console.log(response.data)
   })
   return (<>
    <div className=" flex items-center justify-center h-screen w-screen bg-yellow-300"> Quiz page<Link to="/lectures">Go Back</Link></div>
-             {responseData}
+            
   </> );
 }
 
