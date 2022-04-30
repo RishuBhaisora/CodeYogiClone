c437e7def9df56715e3f29e5b506e27bf7385dee
Rishabh  Bhaisora <102508172+RishuBhaisora@users.noreply.github.com>
2022-04-30 08:44:22 +0000

    used usefeield in Input component and used Formik and Form tag in profile page ,but still one edge the button is not working

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import {useField} from 'formik';
 
-const Input= ({ id, name, type,holder,value,onChange }) => {
+const Input= ({ id, name, type,holder}) => {
+  const [field,meta] = useField(name);
+  
 	return (
 		<div className=" bg-white p-2 items-center border-b-2 border-gray-300 space-x-4 flex">
       <div className="w-1/2">{ holder}</div>
+      {meta.error && meta.touched && (
+						<h2 className="text-red-400">({meta.error})</h2>
+					)}
 			<input
-        value={value}
-        onChange={onChange}
+        {...field}
 				id={id}
 				name={name}
-				type={type}
+				type={type || "text"}
 				placeholder={holder}
 				className=" p-2 border-b-2 border-rose-700 rounded-md w-full "
 			/>
