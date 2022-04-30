7e8f26d8df06c5cae881c1901f0dac3ba79e083b
Rishabh  Bhaisora <102508172+RishuBhaisora@users.noreply.github.com>
2022-04-30 11:04:33 +0000

    fixed errors of onSumit and used validation ,now submiision of the updated data by api is to do ,

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import {useField} from 'formik';
+import { useField } from 'formik';
 
-const Input= ({ id, name, type,holder}) => {
-  const [field,meta] = useField(name);
-  
+const Input = ({ id, name, type, holder,...rest }) => {
+	const [field, meta] = useField(name);
 	return (
 		<div className=" bg-white p-2 items-center border-b-2 border-gray-300 space-x-4 flex">
-      <div className="w-1/2">{ holder}</div>
-      {meta.error && meta.touched && (
-						<h2 className="text-red-400">({meta.error})</h2>
-					)}
+			<div className="w-1/2">{holder}</div>
+			{meta.error &&
+				meta.touched && <h2 className="text-red-400">({meta.error})</h2>}
 			<input
-        {...field}
+				{...field}
+        {...rest}
 				id={id}
 				name={name}
-				type={type || "text"}
+				type={type}
 				placeholder={holder}
 				className=" p-2 border-b-2 border-rose-700 rounded-md w-full "
 			/>
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,97 +1,86 @@
 import React from 'react';
 import Input from './Input';
 import Button from './Button';
-import { Formik ,Form } from 'formik';
+import { Formik , Form } from 'formik';
 import { DateTime } from 'luxon';
-import {object,string } from "yup";
+import { object, string,number } from 'yup';
 
-const Profile= ({profileData}) => {
+const Profile = ({ profileData }) => {
   
-  
- const validationSchema=object().shape({
-      email: string().required().email(),
-      name: string().required(),
-    });
-  const onSubmit = (event) => {
-		 console.log('hiii',event);
-    
-	};
-
-  const dateString = profileData.date_of_birth;
+	const validationSchema = object().shape({
+    first_name: string().required().max(12),
+    last_name: string().required().max(15),
+    email: string().required().email(),
+    year_of_pass_out: number().positive(),
+    phone_no: number().required().positive(),
+    branch: string().required(),
+  })
+	 const onSubmit=()=>{
+     console.log("tycf")
+   }
+	const dateString = profileData.date_of_birth;
 	const dateObject = DateTime.fromISO(dateString);
 	const dateHumanReadable = dateObject.toLocaleString(DateTime.DATE_SHORT);
-  const initialValues= {
-				first_name:profileData.first_name,
-				last_name: profileData.last_name,
-        email:profileData.email,
-        branch: profileData.branch,
-        year_of_pass_out: profileData.year_of_pass_out,
-        phone_no: profileData.phone_no,
-        date_of_birth:dateHumanReadable,
-        
-			}
-  
-	
+	const initialValues = {
+		first_name: profileData.first_name,
+		last_name: profileData.last_name,
+		email: profileData.email,
+		branch: profileData.branch,
+		year_of_pass_out: profileData.year_of_pass_out,
+		phone_no: profileData.phone_no,
+		date_of_birth: dateHumanReadable
+	};
 
 	return (
-    <Formik  onSubmit={onSubmit} initialValues={initialValues} validationSchema={validationSchema}  >
-     
-		<div className="bg-white w-full">
-       <Form  >
-			<h1 className="text-2xl font-bold p-2 bg-green-200  border-b-2 border-gray-700">
-				Personal Detail
-			</h1>
+		<Formik
+			initialValues={initialValues}
+      onSubmit={onSubmit}
 			
-				<Input
-					key="first_name"
-					name="first_name"
-					holder=" First name "
-				/>
+			validationSchema={validationSchema}
+		>
+			<div className="bg-white w-full">
+				<Form>
+					<h1 className="text-2xl font-bold p-2 bg-green-200  border-b-2 border-gray-700">
+						Personal Detail
+					</h1>
 
-				<Input
-					key="last_name"
-					name="last_name"
-					holder=" last name  "
-				/>
+					<Input key="first_name" name="first_name" holder=" First name " />
 
-				<Input
-					key="email"
-					name="email"
-					type="email"
-					holder=" Email address "
-				/>
+					<Input key="last_name" name="last_name" holder=" last name  " />
 
-				<Input
-					key="branch"
-					name="branch"
-					holder="Institute Name "
-				/>
+					<Input
+						key="email"
+						name="email"
+						type="email"
+						holder=" Email address "
+					/>
 
-				<Input
-					key="year_of_pass_out"
-					name="year_of_pass_out"
-					holder=" Year off pass out  "
-				/>
+					<Input key="branch" name="branch" holder="Institute Name " />
 
-				<Input
-					key="phone_no"
-					name="phone_no"
-          type="number"
-					holder=" Phone number "
-				/>
-				<Input
-					key="date_of_birth"
-					name="date_of_birth"
-					holder=" Date of birth "
-				/>
+					<Input
+						key="year_of_pass_out"
+						name="year_of_pass_out"
+						holder=" Year off pass out  "
+					/>
 
-				<div className="p-4 border-b-2 border-gray-300 ">
-					<Button type="submit"  input="Update" />
-				</div>
-			   </Form>
-		</div>
-     
-      </Formik>
+					<Input
+						key="phone_no"
+						name="phone_no"
+						type="number"
+						holder=" Phone number "
+					/>
+					<Input
+						key="date_of_birth"
+						name="date_of_birth"
+						holder=" Date of birth "
+					/>
+
+					<div className="p-4 border-b-2 border-gray-300 ">
+						<Button type="submit" input="Update" />
+					</div>
+				</Form>
+			</div>
+		</Formik>
 	);
 };
 
